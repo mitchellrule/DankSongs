a136d7a033220c91f304c0d37806d732f498210e
Mitch Rule <mitchman5506@gmail.com>
2016-08-12 14:01:47 +1000

    Added voting app

--- voting/models.py
+++ voting/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from vote.managers import VotableManager
 
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=100)
     added_date = models.DateTimeField(blank=True, null=True)
+    votes = VotableManager()
 
     def publish(self):
         self.added_date = timezone.now()
