3515ccd81192344e68430f0e4c9bcb46492ea927
Mitch Rule <mitchman5506@gmail.com>
2016-08-16 21:17:05 +1000

    Added secretballot

--- DankSongs/settings.py
+++ DankSongs/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'voting',
-    'vote',
+    'secretballot',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -50,6 +50,8 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'secretballot.middleware.SecretBallotIpMiddleware',
+    'secretballot.middleware.SecretBallotIpMiddleware',
 ]
 
 ROOT_URLCONF = 'DankSongs.urls'
--- voting/models.py
+++ voting/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.utils import timezone
-from vote.managers import VotableManager
+import secretballot
 
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=100)
     added_date = models.DateTimeField(blank=True, null=True)
-    votes = VotableManager()
 
     def publish(self):
         self.added_date = timezone.now()
@@ -15,3 +14,5 @@ class Song(models.Model):
 
     def __str__(self):
         return self.title
+
+secretballot.enable_voting_on(Song)
